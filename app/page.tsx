5f86f4e066d6f49923af8dfaa754f0c3a2e56ce1
hamrasidi7-rgb <hamrasidi7@gmail.com>
2026-06-12 05:44:54 +0700

    feat: hapus section AI PUTR dari halaman utama

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Header from '@/components/Header'
 import BottomNav from '@/components/BottomNav'
 import HeroKadis from '@/components/HeroKadis'
-import ChatAIPUTR from '@/components/ChatAIPUTR'
 import Footer from '@/components/Footer'
 import WhatsAppButton from '@/components/WhatsAppButton'
 
@@ -12,10 +11,6 @@ export default function HomePage() {
 
       <main className="pb-20 lg:pb-0 min-h-screen">
         <HeroKadis />
-
-        <div className="max-w-[1280px] mx-auto lg:px-8">
-          <ChatAIPUTR />
-        </div>
       </main>
 
       <Footer />
